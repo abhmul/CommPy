1566194098befdc5c1a8b9caf4673370d13172c4
jet457 <abhmul@gmail.com>
2021-10-14 14:52:15 -0500

    remove dead code
    puncture_matrix is defined on line 500 if it is None, so there is no point in having any logic later that runs in puncture_matrix is None

--- commpy/channelcoding/convcode.py
+++ commpy/channelcoding/convcode.py
@@ -520,11 +520,8 @@ def conv_encode(message_bits, trellis, termination = 'term', puncture_matrix=Non
             number_outbits = int(number_inbits/rate)
 
     outbits = np.zeros(number_outbits, 'int')
-    if puncture_matrix is not None:
-        p_outbits = np.zeros(number_outbits, 'int')
-    else:
-        p_outbits = np.zeros(int(number_outbits*
-            puncture_matrix[0:].sum()/np.size(puncture_matrix, 1)), 'int')
+    
+    p_outbits = np.zeros(number_outbits, 'int')
     next_state_table = trellis.next_state_table
     output_table = trellis.output_table
 
